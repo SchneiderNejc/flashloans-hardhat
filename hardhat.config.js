52cc65e610e1f424f3283b38bed94f185d753f75
Nejc Schneider <nejc.sch@gmail.com>
2024-07-31 13:21:11 +0200

    use several solc for the project

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,11 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: {
+    compilers: [
+      { version: "0.5.5" },
+      { version: "0.6.6" },
+      { version: "0.8.8" },
+    ],
+  },
 };
