3c4d2abceabe6ed64e4333e8625beab53a7f08de
Nejc Schneider <nejc.sch@gmail.com>
2024-08-02 15:54:44 +0200

    imports & fixed pragma

--- contracts/FlashSwap.sol
+++ contracts/FlashSwap.sol
@@ -1,9 +1,17 @@
-pragma solidity ^0.6.6;
+pragma solidity 0.6.6;
 
 import "hardhat/console.sol";
 
-// Ujniswap interface and library imports
+// Uniswap interface and library imports
+import "./libraries/UniswapV2Library.sol";
+import "./interfaces/IUniswapV2Router01.sol";
+import "./interfaces/IUniswapV2Router02.sol";
+import "./interfaces/IUniswapV2Pair.sol";
+import "./interfaces/IUniswapV2Factory.sol";
+import "./interfaces/IERC20.sol";
+import "./libraries/SafeERC20.sol";
 
 contract FlashSwap {
+    using SafeERC20 for IERC20;
     
 }
