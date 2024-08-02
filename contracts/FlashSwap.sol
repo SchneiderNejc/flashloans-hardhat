c34e208c8ffc184bd1a17997df84e2f024baf410
Nejc Schneider <nejc.sch@gmail.com>
2024-08-02 16:02:27 +0200

    init addresses

--- contracts/FlashSwap.sol
+++ contracts/FlashSwap.sol
@@ -13,5 +13,16 @@ import "./libraries/SafeERC20.sol";
 
 contract FlashSwap {
     using SafeERC20 for IERC20;
+
+    // Factory and Routing address
+    address constant PANCAKE_FACTORY =
+        0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73;
+    address constant PANCAKE_ROUTER =
+        0x10ED43C718714eb63d5aA57B78B54704E256024E;
+
+    address private constant WBNB = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
+    address private constant BUSD = 0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56;
+    address private constant USDC = 0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d;
+    address private constant CAKE = 0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82;
     
 }
