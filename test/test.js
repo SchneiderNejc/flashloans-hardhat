801b73ec597ce861be30eab35feba533c8bc28a4
Nejc Schneider <nejc.sch@gmail.com>
2024-08-04 13:12:21 +0200

    global variables and dependencies

--- test/test.js
+++ test/test.js
@@ -2,9 +2,21 @@ const { expect, assert } = require("chai");
 const { ethers } = require("hardhat");
 const { impersonateFundErc20 } = require("../utils/utilities");
 
-describe("Token Contract", () => {
-    it("prints hello", () => {
-        console.log("hello");
+const { abi } = require(".../artifacts/contracts/interfaces/IERC20.sol/IERC20.json");
 
+const provider = waffle.provider;
+
+describe("FlashSwap Contract", () => {
+    let FLASHSWAP, BORROW_AMOUNT, FUND_AMOUNT, initiateFundHuman, txArbitrage, gasUsedUSD;
+    
+    const BUSD_WHALE;
+    const BUSD;
+    const USDT;
+    const CAKE;
+    const CROX;
+
+    const BASE_TOKEN_ADDRESS = BUSD;
+
+    const tokenBase = new ethers.Contract(BASE_TOKEN_ADDRESS, abi, provider);
     });
 });
